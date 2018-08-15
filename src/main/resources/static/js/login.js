d6367d4217342d794d0e2bf86f40a8c6fbf65e6e
KimKyuNam <tramyu@naver.com>
2018-08-15 16:20:45 +0900

    Implements login backend feature #12
    login 프론트엔드 기능 추가 구현, js 파일 추가

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,10 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    new Login();
+})
+
 class Login {
     constructor() {
         this.loginButton = $('#loginButton');
         this.emailInput = $('#email');
         this.passwordInput = $('#password');
 
-        this.loginButton.addEventListener('click', this.handlerLoginEvent().bind(this))
+        this.loginButton.addEventListener('click', this.handlerLoginEvent.bind(this));
     }
 
     handlerLoginEvent(evt) {
@@ -29,7 +33,23 @@ class Login {
         document.location.href = '/';
     }
 
-    loginFailureCallback() {
+    loginFailureCallback(response) {
+        const status = response.status;
+        response.json().then((response) => {
+            if(status === 500) {
+                //아이디 또는 비밀번호가 잘못된 경우
+                const validationEl = $('#password-validation');
+                validationEl.style.display = 'block';
+                validationEl.innerHTML = response.message;
+            } else if(status === 400) {
+                //비밀번호나 이메일 양식이 맞지 않는 경우
+                response.errors.forEach((error) => {
+                    const validationEl = $(`#${error.fieldName}-validation`);
+                    validationEl.style.display = 'block';
+                    validationEl.innerHTML = error.errorMessage;
+                });
 
+            }
+        });
     }
 }
