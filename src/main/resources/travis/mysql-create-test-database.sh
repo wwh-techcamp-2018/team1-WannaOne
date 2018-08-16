158231c756f5a3928ae8f6df40925ce402f2577f
KimKyuNam <tramyu@naver.com>
2018-08-16 19:51:46 +0900

    Update travis.yml and application.properties

--- src/main/resources/travis/mysql-create-test-database.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Create woowanote DB"
-mysql -u root -e 'CREATE DATABASE IF NOT EXISTS woowanote;'
--- src/main/resources/travis/mysql-reset-root-password.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-echo "edit root password"
-sudo mysql -e "use mysql; update user set authentication_string=PASSWORD('') where User='root'; update user set plugin='mysql_native_password';FLUSH PRIVILEGES;"
-sudo service mysql restart
