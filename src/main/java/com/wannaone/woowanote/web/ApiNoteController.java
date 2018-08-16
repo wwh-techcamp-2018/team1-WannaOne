f2f5b22231659f3a890a0dfe722566e19ea5d48a
dain7672 <dain7672@gmail.com>
2018-08-16 16:51:38 +0900

    Implements Note posting feature backend 최소의 기능만 먼저구현 아무나 post 가능.

--- src/main/java/com/wannaone/woowanote/web/ApiNoteController.java
+++ src/main/java/com/wannaone/woowanote/web/ApiNoteController.java
@@ -1,12 +1,12 @@
 package com.wannaone.woowanote.web;
 
 import com.wannaone.woowanote.domain.Note;
+import com.wannaone.woowanote.repository.NoteRepository;
 import com.wannaone.woowanote.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/notes")
@@ -15,8 +15,17 @@ public class ApiNoteController {
     @Autowired
     private NoteService noteService;
 
+    @Autowired
+    private NoteRepository noteRepository;
+
     @GetMapping("/{id}")
     public Note show(@PathVariable Long id) {
         return  noteService.getNote(id);
     }
+
+    @PostMapping
+    public ResponseEntity post(@RequestBody Note note) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(noteRepository.save(note).getId());
+    }
+
 }
