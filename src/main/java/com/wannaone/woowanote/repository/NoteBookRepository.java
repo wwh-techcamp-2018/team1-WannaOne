a368c7ee6571cf3235baebb78b79c267a15fb286
doooyeon <ehdus5958@naver.com>
2018-08-17 15:13:39 +0900

    Implements create notebook feature in BE
    - 노트북 생성 controller, service, repository 구현

---
+++ src/main/java/com/wannaone/woowanote/repository/NoteBookRepository.java
@@ -0,0 +1,9 @@
+package com.wannaone.woowanote.repository;
+
+import com.wannaone.woowanote.domain.NoteBook;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface NoteBookRepository extends CrudRepository<NoteBook, Long> {
+}
