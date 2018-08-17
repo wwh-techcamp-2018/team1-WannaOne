16ea39ba831f2c8f897e1c1a198d158011faa84e
doooyeon <ehdus5958@naver.com>
2018-08-18 01:42:45 +0900

    Implements show notebook list feature in FE

--- src/main/resources/static/js/notebookList.js
+++ src/main/resources/static/js/notebookList.js
@@ -1,44 +1,22 @@
 class NoteBookList {
     constructor() {
-        this.noteBookList = $('.sidebar-elements');
-        this.addNotebookTitleInputButton = $('#add-notebook-title-input-btn');
-        this.removeNotebookTitleInputButton = $('#remove-notebook-title-input-btn');
-        this.notebookTitleInputWrapper = $('.notebook-title-input-wrapper');
-        this.notebookTitleInput = $('#notebook-title-input');
-
-        this.addNotebookTitleInputButton.addEventListener('click', this.handlerAddNoteBookTitleInputEvent.bind(this));
-        this.removeNotebookTitleInputButton.addEventListener('click', this.handlerRemoveNoteBookTitleInputEvent.bind(this));
-        this.notebookTitleInput.addEventListener('keyup', this.handlerAddNoteBookEvent.bind(this));
+        this.noteBookList = $('.notebook-list');
+        this.initNoteBookList();
     }
 
-    handlerAddNoteBookTitleInputEvent(evt) {
-        this.notebookTitleInputWrapper.style.display = "block";
+    initNoteBookList() {
+        fetchManager({
+            url: '/api/notebooks',
+            method: 'GET',
+            onSuccess: this.getNoteBookSuccessCallback.bind(this),
+            onFailure: this.getNoteBookFailCallback
+        })
     }
 
-    handlerRemoveNoteBookTitleInputEvent(evt) {
-        this.notebookTitleInputWrapper.style.display = "none";
-    }
-
-    handlerAddNoteBookEvent(evt) {
-        if(evt.keyCode === 13) {
-            const notebookTitle = this.notebookTitleInput.value;
-            if(notebookTitle === '') return;
-            fetchManager({
-                url: '/api/notebooks',
-                method: 'POST',
-                headers: {'content-type': 'application/json'},
-                body: JSON.stringify({
-                    title: notebookTitle
-                }),
-                onSuccess: this.addNoteBookSuccessCallback.bind(this),
-                onFailure: this.addNoteBookFailureCallback
-            });
-        }
-    }
-
-    addNoteBookSuccessCallback(notebook) {
-        this.addNoteBook(notebook);
-        this.clearInput();
+    getNoteBookSuccessCallback(notebookList) {
+        notebookList.forEach((notebook) => {
+            this.addNoteBook(notebook);
+        });
     }
 
     addNoteBook(notebook) {
@@ -47,15 +25,11 @@ class NoteBookList {
 
     noteBookListTemplate(notebook) {
         return `<li data-notebook-id=${notebook.id}></li>
-                <li class="parent"><a href="#"><i class="icon mdi mdi-face"></i><span>${notebook.title}</span></a></li>`;
+                <li>${notebook.title}</li>`;
     }
 
-    clearInput() {
-        this.notebookTitleInput.value = '';
-    }
-
-    addNoteBookFailureCallback() {
-        console.log("노트북 생성 실패")
+    getNoteBookFailCallback() {
+        console.log("노트북 받아오기 실패")
     }
 }
 
