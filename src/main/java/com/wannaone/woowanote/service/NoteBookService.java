abdd75262c502a18310da9b0d201a581b75ff5f6
doooyeon <ehdus5958@naver.com>
2018-08-18 01:42:36 +0900

    Implements show notebook list feature in BE

--- src/main/java/com/wannaone/woowanote/service/NoteBookService.java
+++ src/main/java/com/wannaone/woowanote/service/NoteBookService.java
@@ -12,6 +12,10 @@ public class NoteBookService {
     @Autowired
     private NoteBookRepository noteBookRepository;
 
+    public List<NoteBook> findAll() {
+        return noteBookRepository.findAll();
+    }
+
     public NoteBook save(NoteBook noteBook) {
         return noteBookRepository.save(noteBook);
     }
