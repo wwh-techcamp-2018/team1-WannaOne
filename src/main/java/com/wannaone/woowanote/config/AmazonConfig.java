91d4193b8fbf6b052e20a0e01c33a8999773b456
KimKyuNam <tramyu@naver.com>
2018-08-18 17:31:55 +0900

    Update gitignore and PropertySource #14

--- src/main/java/com/wannaone/woowanote/config/AmazonConfig.java
+++ src/main/java/com/wannaone/woowanote/config/AmazonConfig.java
@@ -2,8 +2,10 @@ package com.wannaone.woowanote.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @EnableConfigurationProperties(AmazonBucketProperties.class)
+@PropertySource(("classpath:aws.properties"))
 public class AmazonConfig {
 }
