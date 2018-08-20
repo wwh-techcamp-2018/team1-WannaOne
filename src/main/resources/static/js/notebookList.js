be4519ac98a68c992717fd44b0650a47396cf17c
doooyeon <ehdus5958@naver.com>
2018-08-20 16:42:02 +0900

    Implements notebook add feature in FE #30

--- src/main/resources/static/js/notebookList.js
+++ src/main/resources/static/js/notebookList.js
@@ -1,9 +1,16 @@
 class NoteBookList {
     constructor() {
         this.noteBookList = $('.notebook-list');
+        this.addNotebookInputButton = $('#add-notebook-input-btn');
+        this.removeNotebookInputButton = $('#remove-notebook-input-btn');
+        this.notebookInputWrapper = $('.notebook-input-wrapper');
+        this.notebookTitleInput = $('#notebook-title-input');
+
         this.initNoteBookList();
+        this.initEventListener();
     }
 
+    // 노트북 목록 조회
     initNoteBookList() {
         fetchManager({
             url: '/api/notebooks',
@@ -29,6 +36,51 @@ class NoteBookList {
 
     getNoteBookFailCallback() {
         console.log("노트북 받아오기 실패")
+    }
+
+    // 노트북 추가
+    initEventListener() {
+        this.addNotebookInputButton.addEventListener('click', this.handlerAddNoteBookInputEvent.bind(this));
+        this.removeNotebookInputButton.addEventListener('click', this.handlerRemoveNoteBookInputEvent.bind(this));
+        this.notebookTitleInput.addEventListener('keyup', this.handlerAddNoteBookEvent.bind(this));
+    }
+
+    handlerAddNoteBookInputEvent(evt) {
+        this.notebookInputWrapper.style.display = "block";
+    }
+
+    handlerRemoveNoteBookInputEvent(evt) {
+        this.notebookInputWrapper.style.display = "none";
+    }
+
+    handlerAddNoteBookEvent(evt) {
+        if(evt.keyCode === 13) {
+            const notebookTitle = this.notebookTitleInput.value;
+            if(notebookTitle === '') return;
+            fetchManager({
+                url: '/api/notebooks',
+                method: 'POST',
+                headers: {'content-type': 'application/json'},
+                body: JSON.stringify({
+                    title: notebookTitle
+                }),
+                onSuccess: this.addNoteBookSuccessCallback.bind(this),
+                onFailure: this.addNoteBookFailureCallback
+            });
+        }
+    }
+
+    addNoteBookSuccessCallback(notebook) {
+        this.addNoteBook(notebook);
+        this.clearInput();
+    }
+
+    clearInput() {
+        this.notebookTitleInput.value = '';
+    }
+
+    addNoteBookFailureCallback() {
+        console.log("노트북 추가 실패")
     }
 }
 
