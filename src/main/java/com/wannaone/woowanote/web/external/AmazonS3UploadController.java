51d2b01484e2a8efdcb73d985be2ea866ef34c3b
KimKyuNam <tramyu@naver.com>
2018-08-20 13:56:41 +0900

    Update AmazonS3UploadController
    미사용 Bean 제거

--- src/main/java/com/wannaone/woowanote/web/external/AmazonS3UploadController.java
+++ src/main/java/com/wannaone/woowanote/web/external/AmazonS3UploadController.java
@@ -2,7 +2,6 @@ package com.wannaone.woowanote.web.external;
 
 
 import com.wannaone.woowanote.common.S3Uploader;
-import com.wannaone.woowanote.config.AmazonBucketProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,8 +11,6 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/aws")
 public class AmazonS3UploadController {
-    @Autowired
-    private AmazonBucketProperties amazonBucketProperties;
     @Autowired
     private S3Uploader s3Uploader;
 
