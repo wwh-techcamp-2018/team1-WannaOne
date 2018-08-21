36e665b9309f22f9f5cc876b5d64e016cc917d2d
KimKyuNam <tramyu@naver.com>
2018-08-21 15:48:35 +0900

    Remove import JpaRepository
    코드리뷰 반영, 미사용 JpaRepository 제거

--- src/main/java/com/wannaone/woowanote/repository/CommentRepository.java
+++ src/main/java/com/wannaone/woowanote/repository/CommentRepository.java
@@ -1,7 +1,6 @@
 package com.wannaone.woowanote.repository;
 
 import com.wannaone.woowanote.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CommentRepository extends CrudRepository<Comment, Long> {
