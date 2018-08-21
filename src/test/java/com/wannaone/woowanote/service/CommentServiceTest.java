d2864cfe7fba75fb4261de5481a9b7a217a213fc
KimKyuNam <tramyu@naver.com>
2018-08-21 16:24:11 +0900

    Remove unnecessary import

--- src/test/java/com/wannaone/woowanote/service/CommentServiceTest.java
+++ src/test/java/com/wannaone/woowanote/service/CommentServiceTest.java
@@ -4,14 +4,11 @@ import com.wannaone.woowanote.domain.Comment;
 import com.wannaone.woowanote.domain.Note;
 import com.wannaone.woowanote.dto.CommentDto;
 import com.wannaone.woowanote.repository.CommentRepository;
-import com.wannaone.woowanote.repository.NoteRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-
-import java.util.Optional;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
