3ca2b790b483500fc7a5504e6217315a9a96c9b6
KimKyuNam <tramyu@naver.com>
2018-08-22 13:57:34 +0900

    Implements Notebook bug fix #76 (#80)
    기존 템플릿 관련 함수들 template-util로 이동

---
+++ src/main/resources/static/js/template-util.js
@@ -0,0 +1,20 @@
+function getNoteSectionTemplate(data, registerDatetime) {
+    return `<input id="note-section-note-title" data-note-id=${data.id} value="${data.title}"></input>
+                <p id="note-section-meta">${registerDatetime}</p>`;
+}
+
+function getNoteBookListTemplate(notebook) {
+    return `<li data-notebook-id=${notebook.id}>${notebook.title}</li>`;
+}
+
+function getNoteItemTemplate(note, registerDatetime) {
+    return `<li data-note-id="${note.id}">
+                <div class="note-item">`
+        + getNoteItemContentTemplate(note, registerDatetime)
+        + `</div></li>`;
+}
+
+function getNoteItemContentTemplate(note, datetime) {
+    return `<div class="note-list-title">${note.title}</div>
+               <div class="note-list-snippet"><span>${datetime} </span>${note.text}</div>`;
+}
