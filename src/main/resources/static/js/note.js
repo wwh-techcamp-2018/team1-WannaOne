53bc5dcfadb321bb862f696466a30fc01c2f09e7
dooho <dooho.hyung@gmail.com>
2018-08-22 14:09:24 +0900

    Implement delete ajax request in FE

--- src/main/resources/static/js/note.js
+++ src/main/resources/static/js/note.js
@@ -7,6 +7,7 @@ class Note {
         this.editor = $('.te-ww-container .tui-editor-contents')
         this.noteListSection = $('.note-list');
         this.noteSaveButton = $('#note-save-button');
+        this.noteDeleteButton = $('#note-delete-button');
         this.initNote();
         this.initButton();
     }
@@ -32,6 +33,7 @@ class Note {
     renderNote(data) {
         this.noteSection.insertAdjacentHTML('beforeend', this.noteSectionFormatter(data));
         this.editor.innerHTML = data.text;
+        this.noteId = data.id;
     }
 
     noteSectionFormatter(data) {
@@ -54,15 +56,15 @@ class Note {
     initButton() {
 //        this.noteSaveButton.addEventListener("click", this.saveHandler.bind(this));
         this.noteSaveButton.addEventListener("click", () => this.updateHandler());
+        this.noteDeleteButton.addEventListener("click", () => this.deleteHandler());
     }
 
     // 노트 수정
     updateHandler() {
-        const noteId = $("#note-section-note-title").dataset.noteId;
         const title = $("#note-section-note-title").value;
         const text = this.editor.innerHTML;
         fetchManager({
-                    url: `/api/notes/${noteId}`,
+                    url: `/api/notes/${this.noteId}`,
                     method: 'PUT',
                     headers: {'content-type': 'application/json'},
                     body: JSON.stringify(
@@ -71,7 +73,7 @@ class Note {
                             text: text
                         }),
                     onSuccess: this.noteUpdateSuccessCallback.bind(this),
-                    onFailure: this.NoteUpdateFailHandler
+                    onFailure: this.noteUpdateFailHandler
                 })
     }
 
@@ -79,7 +81,7 @@ class Note {
         noteList.updateNoteItem(data);
     }
 
-    NoteUpdateFailHandler() {
+    noteUpdateFailHandler() {
         console.log('노트 수정에 실패했습니다.');
     }
 
@@ -110,6 +112,25 @@ class Note {
 
     postNoteFailHandler() {
         console.log('노트 작성에 실패했습니다.');
+    }
+
+    deleteHandler() {
+        fetchManager({
+            url: `/api/notes/${this.noteId}`,
+            method: 'DELETE',
+            onSuccess: this.noteDeleteSuccessCallback.bind(this),
+            onFailure: this.noteDeleteFailHandler
+        })
+    }
+
+    noteDeleteSuccessCallback() {
+        //TODO 1) 노트 리스트에 노트가 남아있다면 삭제한 노트의 다음 노트 선택
+        //TODO 2) 노트가 남아있지 않다면 빈화면
+        console.log('노트 삭제 성공');
+    }
+
+    noteDeleteFailHandler() {
+        console.log('노트 삭제 실패');
     }
 }
 
