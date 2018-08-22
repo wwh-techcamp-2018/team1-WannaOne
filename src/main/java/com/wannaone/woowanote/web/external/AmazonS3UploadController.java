0f79757fb1349a2da78957d4143dd0dcb31c16ba
KimKyuNam <tramyu@naver.com>
2018-08-22 09:08:02 +0900

    Update amazon bucket info
    두호 s3 버킷 정보로 업데이트, 하드코딩 된 static 값 변경

--- src/main/java/com/wannaone/woowanote/web/external/AmazonS3UploadController.java
+++ src/main/java/com/wannaone/woowanote/web/external/AmazonS3UploadController.java
@@ -2,6 +2,7 @@ package com.wannaone.woowanote.web.external;
 
 
 import com.wannaone.woowanote.common.S3Uploader;
+import com.wannaone.woowanote.config.AmazonBucketProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,9 +14,11 @@ import java.io.IOException;
 public class AmazonS3UploadController {
     @Autowired
     private S3Uploader s3Uploader;
+    @Autowired
+    private AmazonBucketProperties amazonBucketProperties;
 
     @PostMapping("/s3upload")
     public String upload(@RequestParam("file") MultipartFile multipartFile) throws IOException {
-        return s3Uploader.upload(multipartFile, "static");
+        return s3Uploader.upload(multipartFile, amazonBucketProperties.getDirectory());
     }
 }
