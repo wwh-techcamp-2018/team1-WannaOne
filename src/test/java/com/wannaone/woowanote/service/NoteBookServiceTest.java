3c20ef062424081b98b3af580cb24026cea4d354
doooyeon <ehdus5958@naver.com>
2018-08-22 11:23:04 +0900

    Update notebook show feature unit test #72

--- src/test/java/com/wannaone/woowanote/service/NoteBookServiceTest.java
+++ src/test/java/com/wannaone/woowanote/service/NoteBookServiceTest.java
@@ -1,6 +1,8 @@
 package com.wannaone.woowanote.service;
 
 import com.wannaone.woowanote.domain.NoteBook;
+import com.wannaone.woowanote.domain.User;
+import com.wannaone.woowanote.dto.UserDto;
 import com.wannaone.woowanote.repository.NoteBookRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -18,6 +20,9 @@ import static org.mockito.Mockito.when;
 @RunWith(MockitoJUnitRunner.class)
 public class NoteBookServiceTest {
     @Mock
+    private UserService userService;
+
+    @Mock
     private NoteBookRepository noteBookRepository;
 
     @InjectMocks
@@ -25,18 +30,25 @@ public class NoteBookServiceTest {
 
     @Test
     public void getAllNoteBook_success() {
+        UserDto loginUserDto = new UserDto("doy@woowahan.com");
+        User loginUser = loginUserDto.toEntity();
+        when(userService.save(loginUserDto)).thenReturn(loginUser);
         NoteBook testNoteBook1 = new NoteBook("노트북1");
         NoteBook testNoteBook2 = new NoteBook("노트북2");
-        when(noteBookService.getAllNoteBooks()).thenReturn(Arrays.asList(testNoteBook1, testNoteBook2));
+        when(noteBookService.getNoteBooksByOwnerId(loginUser.getId())).thenReturn(Arrays.asList(testNoteBook1, testNoteBook2));
 
-        List<NoteBook> noteList = noteBookService.getAllNoteBooks();
+        List<NoteBook> noteList = noteBookService.getNoteBooksByOwnerId(loginUser.getId());
         assertThat(noteList).containsAll(Arrays.asList(testNoteBook1, testNoteBook2));
     }
 
     @Test
     public void getAllNoteBook_success_when_list_empty() {
+        UserDto loginUserDto = new UserDto("doy@woowahan.com");
+        User loginUser = loginUserDto.toEntity();
+        when(userService.save(loginUserDto)).thenReturn(loginUser);
         when(noteBookRepository.findAll()).thenReturn(new ArrayList<>());
-        List<NoteBook> noteBookList = noteBookService.getAllNoteBooks();
+
+        List<NoteBook> noteBookList = noteBookService.getNoteBooksByOwnerId(loginUser.getId());
         assertThat(noteBookList).isEmpty();
     }
 }
