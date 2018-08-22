dd8b80a4092413c31ceb8c99a88e44b17a5cf4ef
dain7672 <dain7672@gmail.com>
2018-08-22 13:33:43 +0900

    Refactor #70 rename member->user note 작성 시  login 안되어있으면 not logined 표시

--- src/main/java/com/wannaone/woowanote/web/ApiNoteController.java
+++ src/main/java/com/wannaone/woowanote/web/ApiNoteController.java
@@ -3,10 +3,12 @@ package com.wannaone.woowanote.web;
 import com.wannaone.woowanote.common.SessionUtil;
 import com.wannaone.woowanote.domain.Note;
 import com.wannaone.woowanote.domain.User;
+import com.wannaone.woowanote.exception.UnAuthenticationException;
 import com.wannaone.woowanote.service.NoteService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import sun.plugin2.message.Message;
+
 import javax.servlet.http.HttpSession;
 import java.util.Optional;
 
@@ -25,6 +29,9 @@ public class ApiNoteController {
 
     @Autowired
     private NoteService noteService;
+
+    @Autowired
+    private MessageSourceAccessor msa;
 
     @GetMapping("/{id}")
     public Note show(@PathVariable Long id) {
@@ -38,7 +45,8 @@ public class ApiNoteController {
 
     @PostMapping("/notebook/{noteBookId}")
     public ResponseEntity<Note> create(@PathVariable Long noteBookId, @RequestBody Note note, HttpSession session) {
-        User writer = (User) session.getAttribute(SessionUtil.USER_SESSION_KEY);
+        User writer = SessionUtil.getUser(session)
+                .orElseThrow(() -> new UnAuthenticationException(msa.getMessage("unauthentication.not.logined")));
         return ResponseEntity.status(HttpStatus.CREATED).body(noteService.save(noteBookId, note, writer));
     }
 
