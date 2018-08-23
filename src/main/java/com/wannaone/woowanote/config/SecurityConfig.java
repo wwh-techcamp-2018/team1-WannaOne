e4efe887424c32aa89f312595f4feee357448566
KimKyuNam <tramyu@naver.com>
2018-08-23 18:28:07 +0900

    Refactor
    remove argumentResolverConfig

--- src/main/java/com/wannaone/woowanote/config/SecurityConfig.java
+++ src/main/java/com/wannaone/woowanote/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package com.wannaone.woowanote.config;
 
 import com.wannaone.woowanote.security.LoginAuthInterceptor;
+import com.wannaone.woowanote.security.LoginUserHandlerMethodArgumentResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class SecurityConfig implements WebMvcConfigurer {
@@ -19,5 +23,15 @@ public class SecurityConfig implements WebMvcConfigurer {
         registry.addInterceptor(loginAuthInterceptor())
                 .addPathPatterns("/main.html")
                 .order(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Bean
+    public LoginUserHandlerMethodArgumentResolver loginUserArgumentResolver() {
+        return new LoginUserHandlerMethodArgumentResolver();
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver());
     }
 }
