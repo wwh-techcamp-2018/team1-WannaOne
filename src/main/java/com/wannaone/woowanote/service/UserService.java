9c97411ebe31e2b7171fdc1868967436ddfefff0
dooho <dooho.hyung@gmail.com>
2018-08-27 21:44:35 +0900

    Add get user methods in user service layer

--- src/main/java/com/wannaone/woowanote/service/UserService.java
+++ src/main/java/com/wannaone/woowanote/service/UserService.java
@@ -40,4 +40,13 @@ public class UserService {
     public boolean isExistUser(String email) {
         return userRepository.findByEmail(email).isPresent();
     }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RecordNotFoundException(msa.getMessage("NotFound.user")));
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new RecordNotFoundException(msa.getMessage("NotFound.user")));
+    }
+
 }
