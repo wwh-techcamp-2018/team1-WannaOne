f3cc088cded039bd0110775d148b934d350f0198
doy <ehdus5958@naver.com>
2018-08-29 17:57:31 +0900

    Feature/reject invitation (#222)
    * Implements invitation reject feature #219
    
    * Update to only receive invitation list in pending state #219
    
    pending 상태인 invitation 리스트만 받아오도록 업데이트
    
    * Update filter grammer
    
    * Update invitation test code
    
    * Update notebook list margin top

--- src/test/java/com/wannaone/woowanote/web/ApiInvitationAcceptanceTest.java
+++ src/test/java/com/wannaone/woowanote/web/ApiInvitationAcceptanceTest.java
@@ -1,7 +1,11 @@
 package com.wannaone.woowanote.web;
 
+import com.wannaone.woowanote.domain.User;
 import com.wannaone.woowanote.dto.InvitationAnswerDto;
+import com.wannaone.woowanote.dto.LoginDto;
+import com.wannaone.woowanote.dto.UserDto;
 import com.wannaone.woowanote.repository.InvitationRepository;
+import com.wannaone.woowanote.service.UserService;
 import com.wannaone.woowanote.support.InvitationStatus;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +17,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ApiInvitationAcceptanceTest extends AcceptanceTest {
     @Autowired
     private InvitationRepository invitationRepository;
+
+    @Autowired
+    private UserService userService;
+
     @Test
     public void acceptInvitationStatus() {
-        InvitationAnswerDto responseDto =  new InvitationAnswerDto(InvitationStatus.ACCEPTED, 1L);
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/invitations", responseDto, Void.class);
+        User loginUser = UserDto.defaultUserDto().setEmail("dain@woowahan.com").toEntity();
+
+        InvitationAnswerDto responseDto =  new InvitationAnswerDto(InvitationStatus.ACCEPTED, 3L);
+        ResponseEntity<Void> response = basicAuthTemplate(loginUser).postForEntity("/api/invitations", responseDto, Void.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-        assertThat(invitationRepository.findById(1L).get().getStatus()).isEqualTo(InvitationStatus.ACCEPTED);
+        assertThat(invitationRepository.findById(3L).get().getStatus()).isEqualTo(InvitationStatus.ACCEPTED);
     }
 }
