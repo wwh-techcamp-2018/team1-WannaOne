91a42d3b3776402d58b55fefb1177c7ab056fb10
dain7672 <dain7672@gmail.com>
2018-08-30 14:05:24 +0900

    Implements redirectioning

--- src/main/java/com/wannaone/woowanote/config/SecurityConfig.java
+++ src/main/java/com/wannaone/woowanote/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(loginAuthInterceptor())
-                .addPathPatterns("/main.html")
+                .addPathPatterns("/main")
                 .order(Ordered.HIGHEST_PRECEDENCE);
     }
 
